9962f070cccc4a572ddaf1872a626341b920b5e3
Stanley Lim <slim679975@gmail.com>
2020-03-25 00:13:50 -0700

    :sparkles: feat(TypeTag): Added colors for food types

--- seattle-takeout/src/components/TypeTag/index.tsx
+++ seattle-takeout/src/components/TypeTag/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import ColorHash from 'color-hash';
 import { Tag } from '../Tag';
+import { TagColors } from '../../model/tag-color';
 
 interface TypeTagProps {
     types: string[];
 }
 
 export const TypeTag: React.FC<TypeTagProps> = (props) => {
-    console.log(props);
-    
+    const colorHash = new ColorHash({ lightness: 0.5 });
+
     return (
         <>
             { props.types.map((type, index) => {
+                const tagColor: TagColors = {
+                    backColor: colorHash.hex(type),
+                    foreColor: '#fff'
+                }
                 return (
-                    <Tag key={index} text={type} />
+                    <Tag key={index} text={type} tagColor={tagColor} />
                 )
             }) }
         </>
