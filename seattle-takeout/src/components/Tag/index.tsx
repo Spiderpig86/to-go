91409c802a0d3aeeba52649c71ea7155562dfd89
Stanley Lim <slim679975@gmail.com>
2020-03-29 17:21:53 -0700

    :rice: feat(data): Fixed up restaurants for other parts of Seattle, tags are now hidden when text is empty

--- seattle-takeout/src/components/Tag/index.tsx
+++ seattle-takeout/src/components/Tag/index.tsx
@@ -11,19 +11,23 @@ interface TagProps {
     setFilterInput: (value: string) => void;
 }
 
-export const Tag: React.FC<TagProps> = (props) => {
-    return (
-        <div className="tag app-tag" css={
-        {
-            backgroundColor: props.tagColor ? `${props.tagColor.backColor} !important` : 'inherit',
-            color: props.tagColor ? `${props.tagColor.foreColor} !important` : 'inherit',
-            cursor: 'pointer',
-            fontSize: '85%',
-            margin: '0.25rem',
-            padding: '0.25rem',
-        }}
-        onClick={(e) => props.setFilterInput(props.text)}>
-            { props.text }
+export const Tag: React.FC<TagProps> = props => {
+    return props.text ? (
+        <div
+            className="tag app-tag"
+            css={{
+                backgroundColor: props.tagColor ? `${props.tagColor.backColor} !important` : 'inherit',
+                color: props.tagColor ? `${props.tagColor.foreColor} !important` : 'inherit',
+                cursor: 'pointer',
+                fontSize: '85%',
+                margin: '0.25rem',
+                padding: '0.25rem'
+            }}
+            onClick={e => props.setFilterInput(props.text)}
+        >
+            {props.text}
         </div>
-    )
-} 
+    ) : (
+        <div></div>
+    );
+};
