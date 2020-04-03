fef8e06ed4de1e6493f8f6cafc13af131e967926
Stanley Lim <slim679975@gmail.com>
2020-04-03 02:39:02 -0700

    :sparkles: feat(yelp): Finished Yelp scraper

--- scripts/yelp/yelp.py
+++ scripts/yelp/yelp.py
@@ -4,26 +4,66 @@ import requests, json, re
 from bs4 import BeautifulSoup
 
 EXISTING_JSON_PATH = '../../data/restaurants.json'
+MAPPING_FILE_NAME = 'mapping'
+
+# Keys
+NAME = 'name'
+TYPES = 'types'
+SERVICES = 'services'
+PHONE = 'phone'
+LOCATION = 'location'
+LOCATIONS = 'locations'
+ADDRESS = 'address'
+WEBSITE = 'website'
+DELIVERY_APPS = 'deliveryApps'
+VEGAN_OPTIONS = 'veganOptions'
+PRICE = 'price'
 
 def main():
     existing_records = None
+    original_map = None
     with open(EXISTING_JSON_PATH, 'r') as f:
         data = f.read()
         data = json.loads(data)
         data = data['restaurants']
-        existing_records = _json_to_map(data, lambda restaurant: 'YELP_URL' in restaurant)
+        existing_records, original_map = _json_to_map(data, lambda restaurant: 'YELP_URL' in restaurant)
+    
+    key_mapping_json = None
+    with open(f'./{MAPPING_FILE_NAME}.json', 'r') as f:
+        mapping_data = f.read()
+        key_mapping_json = json.loads(mapping_data)
 
-    for _, record in existing_records.items():
-        print(existing_records)
+    for key, record in existing_records.items():
         yelp_url = record['YELP_URL']
-        _fetch_fields(yelp_url)
+        yelp_fields = _fetch_fields(yelp_url)
+        transformed = _transform_fields(yelp_fields, key_mapping_json)
+
+        # Merge fields
+        fields_to_merge = [TYPES, LOCATIONS, PRICE] # Modify this
+
+        for fields in fields_to_merge:
+            record[fields] = transformed[fields]
+            
+        del record['YELP_URL']
+        original_map[key] = record
+
+    final_json = {
+        'restaurants': list(original_map.values())
+    }
+    
+    with open(f'./out/updated_yelp_data.json', 'w+') as f:
+        f.write(json.dumps(final_json, indent=4))
+
+    print('Updated Records: ' + str(len(existing_records)))
 
 def _json_to_map(data, _filter=None):
     res = {}
+    original = {}
     for restaurant in data:
         if _filter(restaurant):
             res[restaurant['name']] = restaurant
-    return res
+        original[restaurant['name']] = restaurant
+    return res, original
 
 def _fetch_fields(url):
     response = requests.get(url)
@@ -33,7 +73,34 @@ def _fetch_fields(url):
     json_text = matched_fields[len(matched_fields) - 1].text.replace('<!--', '').replace('-->', '')
 
     fields = json.loads(json_text)
-    print(fields)
+    return fields
+
+def _transform_fields(fields, mapping):
+    price = '$' * _drill_down(fields, mapping[PRICE])[1]
+    types = _drill_down(fields, mapping[TYPES])[1].split(',')
+    types = list(map(lambda t: t.strip().capitalize(), types))
+
+    return {
+        NAME: _drill_down(fields, mapping[NAME]),
+        TYPES: types,
+        SERVICES: ['Curbside/Takeout'],
+        PHONE: _drill_down(fields, mapping[PHONE]),
+        LOCATIONS: [_drill_down(fields, mapping[LOCATION])],
+        ADDRESS: _drill_down(fields, mapping[ADDRESS]),
+        WEBSITE: _drill_down(fields, mapping[WEBSITE]),
+        DELIVERY_APPS: [],
+        VEGAN_OPTIONS: None,
+        PRICE: price
+    }
+
+def _drill_down(fields, property_string):
+    properties = property_string.split('.')
+    temp = fields
+    for prop in properties:
+        if len(prop) == 0:
+            continue
+        temp = temp[prop]
+    return temp
 
 if __name__ == '__main__':
     main()
