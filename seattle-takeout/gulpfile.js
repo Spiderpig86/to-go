209924f317db106ceefc19adc8f4404a1b7216f2
Stanley Lim <slim679975@gmail.com>
2020-04-10 00:27:21 -0700

    :zap: chore(gulp.js): Sped up JSON processing

--- seattle-takeout/gulpfile.js
+++ seattle-takeout/gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const jsonminify = require('gulp-jsonminify');
+const changed = require('gulp-changed');
 
 gulp.task('minify', () => {
     return gulp.src(['../data/restaurants/*.json'])
+        .pipe(changed('../dist/restaurants'))
         .pipe(jsonminify())
         .pipe(gulp.dest('../dist/restaurants'))
 })
